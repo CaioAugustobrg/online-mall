7657a31b1d6f344a6e85398f99073f6fa2a8f69d
Caio Braga <caioaugustobrg@gmail.com>
2023-12-13 01:40:11 -0300

    :sparkles: feat: a get and a method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,22 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/')
   @Render('index')
-  index() {}
+  index() {
+    return {
+      title: 'Home Page - Online Mall',
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['description'] = 'This is an about page...';
+    viewData['author'] = 'Developed by Caio';
+    viewData['title'] = 'About us - Online Mall';
+    viewData['subtitle'] = 'About us';
+    return {
+      viewData: viewData,
+    };
+  }
 }
