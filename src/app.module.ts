b5a0a53e97d12b5b71815c8b9cf00e5d26f833ed
Caio Braga <caioaugustobrg@gmail.com>
2023-12-13 22:41:38 -0300

    :sparkles: feat: imported ProductsService and Product entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './models/product.entity';
+import { ProductsService } from './models/products.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'online_store',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Product]),
+  ],
+  controllers: [AppController, ProductsController],
+  providers: [ProductsService],
 })
 export class AppModule {}
