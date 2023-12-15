5e2b3ec114439a142ebd561d766471637b034771
Caio Braga <caioaugustobrg@gmail.com>
2023-12-15 01:41:56 -0300

    :sparkles: feat: new routes

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Render, Post, Body, Redirect } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Render,
+  Post,
+  Body,
+  Redirect,
+  UseInterceptors,
+  UploadedFile,
+  Param,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductsService } from 'src/models/products.service';
 import { Product } from 'src/models/product.entity';
 
@@ -9,20 +20,55 @@ export class AdminProductsController {
   @Render('admin/products/index')
   async index() {
     const viewData = [];
-    viewData['title'] = 'Admin Page - Admin - Online Store';
+    viewData['title'] = 'Admin Page - Admin - Online Mall';
     viewData['products'] = await this.productsService.findAll();
     return {
       viewData: viewData,
     };
   }
   @Post('/store')
+  @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
   @Redirect('/admin/products')
-  async store(@Body() body) {
+  async store(@Body() body, @UploadedFile() file: Express.Multer.File) {
     const newProduct = new Product();
     newProduct.setName(body.name);
     newProduct.setDescription(body.description);
     newProduct.setPrice(body.price);
-    newProduct.setImage('game.png');
+    newProduct.setImage(file.filename);
     await this.productsService.createOrUpdate(newProduct);
+  }
+
+  @Post('/:id')
+  @Redirect('admin/products')
+  remove(@Param('id') id: number) {
+    return this.productsService.remove(id);
+  }
+
+  @Get('/:id')
+  @Render('admin/products/edit')
+  async edit(@Param('id') id: number) {
+    const viewData = [];
+    viewData['title'] = 'Admin Page - Edit Product - Online Store';
+    viewData['product'] = await this.productsService.findOne(id);
+    return {
+      viewData: viewData,
+    };
+  }
+  @Post('/:id/update')
+  @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
+  @Redirect('/products')
+  async update(
+    @Body() body,
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id') id: number,
+  ) {
+    const product = await this.productsService.findOne(id);
+    product.setName(body.name);
+    product.setDescription(body.description);
+    product.setPrice(body.price);
+    if (file) {
+      product.setImage(file.filename);
+    }
+    await this.productsService.createOrUpdate(product);
   }
 }
