39434960a5b6cda6ead6026203f434658d44a8b7
Caio Braga <caioaugustobrg@gmail.com>
2023-12-21 12:15:53 -0300

    :sparkles: feat: using the validator class and method

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -5,10 +5,13 @@ import {
   Post,
   Body,
   Redirect,
+  Req,
   UseInterceptors,
   UploadedFile,
   Param,
 } from '@nestjs/common';
+import { ProductValidator } from 'src/validator/product.validator';
+import * as fs from 'fs';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductsService } from 'src/models/products.service';
 import { Product } from 'src/models/product.entity';
@@ -29,13 +32,31 @@ export class AdminProductsController {
   @Post('/store')
   @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
   @Redirect('/admin/products')
-  async store(@Body() body, @UploadedFile() file: Express.Multer.File) {
-    const newProduct = new Product();
-    newProduct.setName(body.name);
-    newProduct.setDescription(body.description);
-    newProduct.setPrice(body.price);
-    newProduct.setImage(file.filename);
-    await this.productsService.createOrUpdate(newProduct);
+  async store(
+    @Body() body,
+    @UploadedFile() file: Express.Multer.File,
+    @Req() request,
+  ) {
+    const toValidate: string[] = [
+      'name',
+      'description',
+      'price',
+      'imageCreate',
+    ];
+    const errors: string[] = ProductValidator.validate(body, file, toValidate);
+    if (errors.length > 0) {
+      if (file) {
+        fs.unlinkSync(file.path);
+      }
+      request.session.flashErrors = errors;
+    } else {
+      const newProduct = new Product();
+      newProduct.setName(body.name);
+      newProduct.setDescription(body.description);
+      newProduct.setPrice(body.price);
+      newProduct.setImage(file.filename);
+      await this.productsService.createOrUpdate(newProduct);
+    }
   }
 
   @Post('/:id')
