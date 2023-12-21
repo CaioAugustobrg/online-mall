64da50db75e280e165ab8966cffe772f10d65405
Caio Braga <caioaugustobrg@gmail.com>
2023-12-21 12:16:52 -0300

    :sparkles: feat: if theres an error put it in res.locals

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as session from 'express-session';
 import * as hbs from 'hbs';
 import * as hbsUtils from 'hbs-utils';
+import * as session from 'express-session';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
@@ -15,15 +16,21 @@ async function bootstrap() {
   app.setViewEngine('hbs');
   app.use(
     session({
-      secret: 'nest',
+      secret: 'nest-book',
       resave: false,
       saveUninitialized: false,
     }),
   );
-
   app.use(function (req, res, next) {
     res.locals.session = req.session;
+    const flashErrors: string[] = req.session.flashErrors;
+    if (flashErrors) {
+      res.locals.flashErrors = flashErrors;
+      req.session.flashErrors = null;
+    }
     next();
   });
+
+  await app.listen(3000);
 }
 bootstrap();
